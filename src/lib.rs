09c4b7bc9bc7539f7ce35fb2c014be2c69ef3df1
JustAStream <keirinvestment@gmail.com>
2022-04-02 15:16:06 +0100

    feat: added ExchangeId, MarketStream abstraction, ExchangeTransformer abstraction

--- src/lib.rs
+++ src/lib.rs
@@ -1,141 +1,181 @@
-pub mod client;
-pub mod connection;
-pub mod error;
-pub mod lib_new;
-
-use std::pin::Pin;
-use std::task::{Context, Poll};
-use crate::{
-    error::ClientError,
-    model::{Candle, MarketData, Trade},
-    client::binance::BinanceMessage
-};
-use barter_integration::socket::{
-    Transformer, error::SocketError, ExchangeSocket
-};
+use std::fmt::{Display, Formatter};
+use futures::{SinkExt, Stream};
+use barter_integration::socket::error::SocketError;
+use barter_integration::socket::protocol::websocket::{connect, ExchangeWebSocket, WebSocketParser, WsMessage};
+use barter_integration::socket::{ExchangeSocket, Transformer};
+use crate::model::{MarketEvent, StreamId, Subscription};
+use serde::{Deserialize, Serialize};
 use async_trait::async_trait;
-use futures::{Sink, Stream};
-use serde::de::DeserializeOwned;
-use serde::Serialize;
-use tokio::net::TcpStream;
-use tokio_tungstenite::{
-    connect_async, MaybeTlsStream, WebSocketStream,
-    tungstenite::Message as WsMessage
-};
-use tokio::sync::mpsc::UnboundedReceiver;
-use tracing::debug;
-use barter_integration::socket::protocol::websocket::{WebSocket, WebSocketParser};
 
+pub mod builder;
+pub mod model;
+pub mod binance;
 
-// Todo: general:
-//  - Increase test coverage significantly now you know the PoC design works
-//  - Unsure all .unwrap()s have been exchanged for more robust handling
-//  - Ensure proper error handling & swapping unwraps() for more robust handling
-//     '-> ensure all methods are returning an appropriate Result which is handled by caller
-
-// Todo: connection.rs:
-//  - Improve method of confirming subscription request so test_subscribe unit test passed
-//     '-> subscription succeeded even if it didn't, need to confirm first message arrives?
-//     '-> ensure logging is aligned once this has been done
-//  - manage() add in connection fixing, reconnections
-
-/// Client trait defining the behaviour of all implementing ExchangeClients. All methods return
-/// a stream of normalised data.
-#[async_trait]
-pub trait ExchangeClient {
-    const EXCHANGE_NAME: &'static str;
-    async fn consume_trades(&mut self, symbol: String, ) -> Result<UnboundedReceiver<Trade>, ClientError>;
-    async fn consume_candles(&mut self, symbol: String, interval: &str) -> Result<UnboundedReceiver<Candle>, ClientError>;
+// Todo: Rust docs, add basic impls, change name to ExchangeId ? Produce &'static str
+#[derive(Copy, Clone, Eq, PartialEq, Ord, PartialOrd, Hash, Debug, Deserialize, Serialize)]
+pub enum ExchangeId {
+    BinanceFutures,
+    Binance,
+    Ftx,
 }
 
-/// Utilised to subscribe to an exchange's [`WebSocketStream`] via a ConnectionHandler (eg/ Trade stream).
-pub trait Subscription {
-    /// Constructs a new [`Subscription`] implementation.
-    fn new(stream_name: String, ticker_pair: String) -> Self;
-    /// Serializes the [`Subscription`] in a String data format.
-    fn as_text(&self) -> Result<String, ClientError>
-    where
-        Self: Serialize,
-    {
-        Ok(serde_json::to_string(self)?)
+impl ExchangeId {
+    /// Todo:
+    pub fn as_str(&self) -> &'static str {
+        match self {
+            ExchangeId::Binance => "binance",
+            ExchangeId::BinanceFutures => "binance_futures",
+            ExchangeId::Ftx => "ftx",
+        }
+    }
+
+    /// Todo:
+    pub fn supports_spot(&self) -> bool {
+        match self {
+            ExchangeId::BinanceFutures => false,
+            _ => true,
+        }
+    }
+
+    /// Todo:
+    pub fn supports_futures(&self) -> bool {
+        match self {
+            ExchangeId::BinanceFutures => true,
+            _ => false,
+        }
     }
 }
 
-/// Returns a stream identifier that can be used to route messages from a [`Subscription`].
+impl Display for ExchangeId {
+    fn fmt(&self, f: &mut Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", match self {
+            ExchangeId::BinanceFutures => "binance_futures",
+            ExchangeId::Binance => "binance",
+            ExchangeId::Ftx => "ftx",
+        })
+    }
+}
+
+/// Todo:
 pub trait StreamIdentifier {
-    fn get_stream_id(&self) -> Identifier;
+    fn to_stream_id(&self) -> StreamId;
 }
 
-/// Enum returned from [`StreamIdentifier`] representing if a struct has an identifiable stream Id.
-pub enum Identifier {
-    Yes(String),
-    No,
+/// Todo:
+#[async_trait]
+pub trait MarketStream: Stream<Item = Result<MarketEvent, SocketError>> + Sized + Unpin {
+    async fn init(subscriptions: &[Subscription]) -> Result<Self, SocketError>;
 }
 
-/// Connect asynchronously to an exchange's server, returning a [`WebSocketStream`].
-async fn connect(base_uri: &String) -> Result<WSStream, ClientError> {
-    debug!("Establishing WebSocket connection to: {:?}", base_uri);
-    connect_async(base_uri)
-        .await
-        .and_then(|(ws_stream, _)| Ok(ws_stream))
-        .map_err(|err| ClientError::WebSocketConnect(err))
+/// Todo:
+pub trait ExchangeTransformer: Sized
+where
+    Self: Transformer<MarketEvent>,
+{
+    const EXCHANGE: ExchangeId;
+    const BASE_URL: &'static str;
+    fn new() -> Self;
+    fn generate_subscriptions(&mut self, subscriptions: &[Subscription]) -> Vec<serde_json::Value>;
 }
 
-pub mod test_util {
-    use crate::model::Candle;
-    use chrono::Utc;
+#[async_trait]
+impl<ExchangeT> MarketStream for ExchangeWebSocket<ExchangeT, ExchangeT::Input, MarketEvent>
+where
+    Self: Stream<Item = Result<MarketEvent, SocketError>> + Sized + Unpin,
+    ExchangeT: ExchangeTransformer + Send,
+{
+    async fn init(subscriptions: &[Subscription]) -> Result<Self, SocketError> {
+        // Construct Exchange Transformer to translate between Barter & exchange data structures
+        let mut exchange = ExchangeT::new();
 
-    pub fn candle() -> Candle {
-        Candle {
-            start_timestamp: Utc::now(),
-            end_timestamp: Utc::now(),
-            open: 1000.0,
-            high: 1100.0,
-            low: 900.0,
-            close: 1050.0,
-            volume: 1000000000.0,
-            trade_count: 100,
+        // Connect to exchange WebSocket server
+        let mut websocket = connect(ExchangeT::BASE_URL).await?;
+
+        // Action Subscriptions over the socket
+        for sub_payload in exchange.generate_subscriptions(subscriptions) {
+            websocket
+                .send(WsMessage::Text(sub_payload.to_string()))
+                .await?;
         }
+
+        Ok(ExchangeSocket::new(websocket, WebSocketParser, exchange))
     }
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
 
-    #[tokio::test]
-    async fn test_connect() {
-        struct TestCase {
-            input_base_uri: String,
-            expected_can_connect: bool,
-        }
 
-        let test_cases = vec![
-            TestCase {
-                // Test case 0: Not a valid WS base URI
-                input_base_uri: "not a valid base uri".to_string(),
-                expected_can_connect: false,
-            },
-            TestCase {
-                // Test case 1: Valid Binance WS base URI
-                input_base_uri: "wss://stream.binance.com:9443/ws".to_string(),
-                expected_can_connect: true,
-            },
-            TestCase {
-                // Test case 2: Valid Bitstamp WS base URI
-                input_base_uri: "wss://ws.bitstamp.net/".to_string(),
-                expected_can_connect: true,
-            },
-        ];
 
-        for (index, test) in test_cases.into_iter().enumerate() {
-            let actual_result = connect(&test.input_base_uri).await;
-            assert_eq!(
-                test.expected_can_connect,
-                actual_result.is_ok(),
-                "Test case: {:?}",
-                index
-            );
-        }
-    }
-}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// pub mod test_util {
+//     use crate::model::Candle;
+//     use chrono::Utc;
+//
+//     pub fn candle() -> Candle {
+//         Candle {
+//             start_timestamp: Utc::now(),
+//             end_timestamp: Utc::now(),
+//             open: 1000.0,
+//             high: 1100.0,
+//             low: 900.0,
+//             close: 1050.0,
+//             volume: 1000000000.0,
+//             trade_count: 100,
+//         }
+//     }
+// }
+//
+// #[cfg(test)]
+// mod tests {
+//     use super::*;
+//
+//     #[tokio::test]
+//     async fn test_connect() {
+//         struct TestCase {
+//             input_base_uri: String,
+//             expected_can_connect: bool,
+//         }
+//
+//         let test_cases = vec![
+//             TestCase {
+//                 // Test case 0: Not a valid WS base URI
+//                 input_base_uri: "not a valid base uri".to_string(),
+//                 expected_can_connect: false,
+//             },
+//             TestCase {
+//                 // Test case 1: Valid Binance WS base URI
+//                 input_base_uri: "wss://stream.binance.com:9443/ws".to_string(),
+//                 expected_can_connect: true,
+//             },
+//             TestCase {
+//                 // Test case 2: Valid Bitstamp WS base URI
+//                 input_base_uri: "wss://ws.bitstamp.net/".to_string(),
+//                 expected_can_connect: true,
+//             },
+//         ];
+//
+//         for (index, test) in test_cases.into_iter().enumerate() {
+//             let actual_result = connect(&test.input_base_uri).await;
+//             assert_eq!(
+//                 test.expected_can_connect,
+//                 actual_result.is_ok(),
+//                 "Test case: {:?}",
+//                 index
+//             );
+//         }
+//     }
+// }
