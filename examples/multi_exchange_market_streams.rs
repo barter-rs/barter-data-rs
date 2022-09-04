cd782eee03add9d75ddcb73fa1869767ec5293e7
JustAStream <keirinvestment@gmail.com>
2022-09-04 20:41:27 +0100

    feat: added coinbase to examples & readme code

--- examples/multi_exchange_market_streams.rs
+++ examples/multi_exchange_market_streams.rs
@@ -6,10 +6,12 @@ use barter_data::{
 use barter_integration::model::InstrumentKind;
 use futures::StreamExt;
 
-// StreamBuilder subscribing to various Futures & Spot MarketStreams from Ftx, Kraken & BinanceFuturesUsd
+// StreamBuilder subscribing to various Futures & Spot MarketStreams from Ftx, Kraken,
+// BinanceFuturesUsd & Coinbase
 #[tokio::main]
 async fn main() {
-    // Initialise `PublicTrade` & `Candle``MarketStream` for `BinanceFuturesUsd`, `Ftx` & `Kraken`
+    // Initialise `PublicTrade` & `Candle``MarketStream` for `BinanceFuturesUsd`, `Ftx`, `Kraken`
+    // & Coinbase
     let streams = Streams::builder()
         .subscribe_exchange(
             ExchangeId::Ftx,
@@ -21,11 +23,12 @@ async fn main() {
             ],
         )
         .subscribe([
-            (ExchangeId::Kraken, "xbt", "usd", InstrumentKind::Spot, SubKind::Trade),
-            (ExchangeId::Kraken, "xbt", "usd", InstrumentKind::Spot, SubKind::Candle(Interval::Minute1)),
-            (ExchangeId::Kraken, "eth", "usd", InstrumentKind::Spot, SubKind::Candle(Interval::Minute1)),
+            (ExchangeId::Coinbase, "btc", "usd", InstrumentKind::Spot, SubKind::Trade),
+            (ExchangeId::Coinbase, "eth", "usd", InstrumentKind::Spot, SubKind::Trade),
             (ExchangeId::BinanceFuturesUsd, "btc", "usdt", InstrumentKind::FuturePerpetual, SubKind::Trade),
             (ExchangeId::BinanceFuturesUsd, "eth", "usdt", InstrumentKind::FuturePerpetual, SubKind::Trade),
+            (ExchangeId::Kraken, "xbt", "usd", InstrumentKind::Spot, SubKind::Trade),
+            (ExchangeId::Kraken, "xbt", "usd", InstrumentKind::Spot, SubKind::Candle(Interval::Minute1)),
         ])
         .init()
         .await
