e7695cb80ae5df523ae8e8a048f196d6c3e9c897
JustAStream <just-a-stream@users.noreply.github.com>
2022-12-18 19:15:07 +0000

    feat: binance subs added

--- examples/multi_exchange_market_streams.rs
+++ examples/multi_exchange_market_streams.rs
@@ -1,3 +1,6 @@
+use barter_data::exchange::binance::futures::BinanceFuturesUsd;
+use barter_data::exchange::binance::spot::{BinanceServerSpot, BinanceSpot};
+use barter_data::exchange::binance::Binance;
 use barter_data::exchange::coinbase::Coinbase;
 use barter_data::subscriber::subscription::trade::PublicTrades;
 use barter_data::subscriber::subscription::{SubKind, Subscription};
@@ -7,13 +10,39 @@ use futures::StreamExt;
 
 #[tokio::main]
 async fn main() {
+    // init_logging();
+
     // Subscriptions
     let subscriptions = vec![
-        (Coinbase, "btc", "usd", InstrumentKind::Spot, PublicTrades).into(),
-        (Coinbase, "eth", "usd", InstrumentKind::Spot, PublicTrades).into(),
-        (Coinbase, "btc", "gbp", InstrumentKind::Spot, PublicTrades).into(),
-        (Coinbase, "eth", "gbp", InstrumentKind::Spot, PublicTrades).into(),
-        (Coinbase, "sol", "usdt", InstrumentKind::Spot, PublicTrades).into(),
+        // (Coinbase, "btc", "usd", InstrumentKind::Spot, PublicTrades).into(),
+        // (Coinbase, "eth", "usd", InstrumentKind::Spot, PublicTrades).into(),
+        // (Coinbase, "btc", "gbp", InstrumentKind::Spot, PublicTrades).into(),
+        // (Coinbase, "eth", "gbp", InstrumentKind::Spot, PublicTrades).into(),
+        // (Coinbase, "sol", "usdt", InstrumentKind::Spot, PublicTrades).into(),
+        (
+            BinanceSpot::default(),
+            "btc",
+            "usdt",
+            InstrumentKind::Spot,
+            PublicTrades,
+        )
+            .into(),
+        (
+            BinanceSpot::default(),
+            "eth",
+            "usdt",
+            InstrumentKind::Spot,
+            PublicTrades,
+        )
+            .into(),
+        (
+            BinanceSpot::default(),
+            "xrp",
+            "usdt",
+            InstrumentKind::Spot,
+            PublicTrades,
+        )
+            .into(),
     ];
 
     tokio::spawn(consume(subscriptions)).await.unwrap();
