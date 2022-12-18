569729a9b7b3588abde594c125a3df6343adb36b
JustAStream <just-a-stream@users.noreply.github.com>
2022-12-18 19:14:43 +0000

    feat: add derives

--- src/transformer/book.rs
+++ src/transformer/book.rs
@@ -1,2 +1,7 @@
+use serde::{Deserialize, Serialize};
+
+#[derive(Copy, Clone, Eq, PartialEq, Ord, PartialOrd, Hash, Debug, Deserialize, Serialize)]
 pub struct BookTransformer;
+
+#[derive(Copy, Clone, Eq, PartialEq, Ord, PartialOrd, Hash, Debug, Deserialize, Serialize)]
 pub struct MultiBookTransformer;
