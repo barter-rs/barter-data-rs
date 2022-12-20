e5cc04fc7d4eaa5a9850f9762a310f176967dd6e
JustAStream <just-a-stream@users.noreply.github.com>
2022-12-20 22:46:38 +0000

    refactor: model -> event.rs

--- src/event.rs
+++ src/event.rs
@@ -20,6 +20,9 @@ impl<Event> FromIterator<Result<Market<Event>, SocketError>> for MarketIter<Even
 /// Normalised Barter [`Market<Event>`](Self) containing metadata about the included `Event` variant.
 ///
 /// Note: `Event` can be an enum if required.
+///
+/// eg/ Market<PublicTrade>
+/// eg/ Market<OrderBook<Level3>>
 #[derive(Clone, PartialEq, Eq, PartialOrd, Ord, Debug, Deserialize, Serialize)]
 pub struct Market<Event> {
     pub exchange_time: DateTime<Utc>,
@@ -27,16 +30,4 @@ pub struct Market<Event> {
     pub exchange: Exchange,
     pub instrument: Instrument,
     pub event: Event,
-}
-
-/// Normalised Barter OHLCV [`Candle`] model.
-#[derive(Copy, Clone, PartialEq, PartialOrd, Debug, Deserialize, Serialize)]
-pub struct Candle {
-    pub close_time: DateTime<Utc>,
-    pub open: f64,
-    pub high: f64,
-    pub low: f64,
-    pub close: f64,
-    pub volume: f64,
-    pub trade_count: u64,
 }
