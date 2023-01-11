b0febe81186a81ea06be95a8e7d93259242b86d6
JustAStream <just-a-stream@users.noreply.github.com>
2023-01-11 19:49:18 +0000

    feat: test validate(subs)

--- src/streams/builder.rs
+++ src/streams/builder.rs
@@ -164,3 +164,72 @@ where
 
     Ok(())
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use crate::exchange::coinbase::Coinbase;
+    use crate::subscription::trade::PublicTrades;
+    use barter_integration::model::InstrumentKind;
+
+    #[test]
+    fn test_validate() {
+        struct TestCase {
+            input: Vec<Subscription<Coinbase, PublicTrades>>,
+            expected: Result<Vec<Subscription<Coinbase, PublicTrades>>, SocketError>,
+        }
+
+        let cases = vec![
+            TestCase {
+                // TC0: Invalid Vec<Subscription> w/ empty vector
+                input: vec![],
+                expected: Err(SocketError::Subscribe("".to_string())),
+            },
+            TestCase {
+                // TC1: Valid Vec<Subscription> w/ valid Coinbase Spot sub
+                input: vec![Subscription::from((
+                    Coinbase,
+                    "base",
+                    "quote",
+                    InstrumentKind::Spot,
+                    PublicTrades,
+                ))],
+                expected: Ok(vec![Subscription::from((
+                    Coinbase,
+                    "base",
+                    "quote",
+                    InstrumentKind::Spot,
+                    PublicTrades,
+                ))])
+            },
+            TestCase {
+                // TC2: Invalid StreamBuilder w/ invalid Coinbase FuturePerpetual sub
+                input: vec![Subscription::from((
+                    Coinbase,
+                    "base",
+                    "quote",
+                    InstrumentKind::FuturePerpetual,
+                    PublicTrades,
+                ))],
+                expected: Err(SocketError::Subscribe("".to_string())),
+            },
+        ];
+
+        for (index, test) in cases.into_iter().enumerate() {
+            let actual = validate(&test.input);
+
+            match (actual, test.expected) {
+                (Ok(_), Ok(_)) => {
+                    // Test passed
+                }
+                (Err(_), Err(_)) => {
+                    // Test passed
+                }
+                (actual, expected) => {
+                    // Test failed
+                    panic!("TC{index} failed because actual != expected. \nActual: {actual:?}\nExpected: {expected:?}\n");
+                }
+            }
+        }
+    }
+}
