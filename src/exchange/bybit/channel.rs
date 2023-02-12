5693e987144dc8b4d69bb5b3c667548771aaf2f9
Joel Atherley <joelnatherley@gmail.com>
2023-02-12 16:54:18 +0100

    Add documentation to channel

--- src/exchange/bybit/channel.rs
+++ src/exchange/bybit/channel.rs
@@ -5,9 +5,17 @@ use crate::subscription::Subscription;
 use crate::subscription::trade::PublicTrades;
 
 #[derive(Copy, Clone, Eq, PartialEq, Ord, PartialOrd, Hash, Debug, Serialize)]
+
+/// Type that defines how to translate a Barter [`Subscription`] into a [`Bybit`](super::Bybit)
+/// channel to be subscribed to.
+///
+/// See docs: <https://bybit-exchange.github.io/docs/v5/ws/connect>
 pub struct BybitChannel(pub &'static str);
 
 impl BybitChannel {
+    /// [`Binance`](super::Bybit) real-time trades channel name.
+    ///
+    /// See docs: <https://bybit-exchange.github.io/docs/v5/websocket/public/trade>
     pub const TRADES: Self = Self("publicTrade");
 }
 
