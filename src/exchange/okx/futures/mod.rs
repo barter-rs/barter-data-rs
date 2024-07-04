3bc66a966689ea38a5dd5a1fde7eaf417bcba671
Jacob <jacoblodenius@gmail.com>
2024-07-04 13:26:41 +0200

    move l1 above l2

--- src/exchange/okx/futures/mod.rs
+++ src/exchange/okx/futures/mod.rs
@@ -7,21 +7,21 @@ use crate::{
 };
 use barter_integration::model::instrument::Instrument;
 
+/// Level 1 OrderBook types (top of book) for perpetual futures
+/// [`OrderBookUpdater`](crate::transformer::book::OrderBookUpdater) implementation.
+pub mod l1;
+
 /// Level 2 OrderBook types for perpetual futures
 /// [`OrderBookUpdater`](crate::transformer::book::OrderBookUpdater) implementation.
 pub mod l2;
 
-/// Level 1 OrderBook types (top of book) for perpetual futures
-/// [`OrderBookUpdater`](crate::transformer::book::OrderBookUpdater) implementation.
-pub mod l1;
+impl StreamSelector<Instrument, OrderBooksL1> for Okx {
+    type Stream =
+        ExchangeWsStream<StatelessTransformer<Self, Instrument, OrderBooksL1, OkxOrderBookL1>>;
+}
 
 impl StreamSelector<Instrument, OrderBooksL2> for Okx {
     type Stream = ExchangeWsStream<
         MultiBookTransformer<Self, Instrument, OrderBooksL2, l2::OkxFuturesBookUpdater>,
     >;
-}
-
-impl StreamSelector<Instrument, OrderBooksL1> for Okx {
-    type Stream =
-        ExchangeWsStream<StatelessTransformer<Self, Instrument, OrderBooksL1, OkxOrderBookL1>>;
 }
